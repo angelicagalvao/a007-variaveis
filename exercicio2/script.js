0b0f36b72a8dca0c30db36a8466b01b61ce74172
angelicagalvao <angelicagalvao18@gmail.com>
2022-07-19 18:26:29 -0300

    Exercicio a007

---
+++ exercicio2/script.js
@@ -0,0 +1,15 @@
+const perguntaA = "Você está se sentindo bem hoje?"
+const perguntaB = "Você gosta de macarrão?"
+const perguntaC = "Você gosta de jogar futebol?"
+
+
+const respostaA = prompt(perguntaA);
+const respostaB = prompt(perguntaB);
+const respostaC = prompt(perguntaC);
+const humor = false
+const macarrao = true
+const futebol = true
+console.log(perguntaA, humor)  
+console.log(perguntaB, macarrao)
+console.log(perguntaC, futebol);
+
---
+++ exercicio3/script.js
@@ -0,0 +1,5 @@
+let a = 10
+let b = 20
+
+console.log("O novo valor de a é", b)
+console.log("O novo valor de b é", a)
